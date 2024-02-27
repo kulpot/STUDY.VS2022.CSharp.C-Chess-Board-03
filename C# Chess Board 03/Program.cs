122fc969906b86e4521315ba1c2b54158258dcea
Kulpot <kulpotwahu@gmail.com>
2024-02-27 21:34:36 +0800

    C/P

--- C# Chess Board 03/Program.cs
+++ C# Chess Board 03/Program.cs
@@ -4,7 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace C__Chess_Board_03
+//-------------------- C# Chess Board 02 board cell classes -------------------------------
+//ref link:https://www.youtube.com/watch?v=SFMVyiJ2S6g&list=PLhPyEFL5u-i0YDRW6FLMd1PavZp9RcYdF&index=3
+
+// addNewItemToSolution:class library(name:ClassBoardModel), addNewClassTo:ClassBoardModel(name:Cell.cs/Board.cs),
+
+namespace C__Chess_Board_02
 {
     internal class Program
     {
